f4df11f34def28ec27a86dcea86dd802bb7bb22e
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-10-20 16:06:07 +0100

    Auth0 Access Token done

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,10 +1,14 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 type Props = {
     children: React.ReactNode;
 }
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  
+    const navigate = useNavigate()
+
+
     const domain = import.meta.env.VITE_AUTHO_DOMAIN;
     const clientId = import.meta.env.VITE_AUTHO_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -14,7 +18,8 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     }
 
     const onRedirectCallback = (appState?: AppState, user?: User) => {
-        console.log('USER', user);
+        navigate("/auth-callback")
+       
     }
 
     return (
